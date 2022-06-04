639f24ee9f3abe535ceeea81b8d7278dc5a4bcb9
ibraheem15 <103947216+ibraheem15@users.noreply.github.com>
2022-06-04 16:27:41 +0500

    Update ACTUAL HEADER.h

--- ACTUAL HEADER.h
+++ ACTUAL HEADER.h
@@ -2,144 +2,268 @@
  **  Ibraheem Rehman  **
  **  SE-Q    21i-1102 **
  ***********************/
- #pragma once
+
+#include <iso646.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctime>
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <random>
 #include "util.h"
+using namespace std;
 
-class Map;
-class Menu;
-class Progress;
+#include "ACTUAL HEADER.h"
+#include "SHOOT.h"
+#include "MAP.h"
+#include "CAR.h"
+#include "MOVEMENT.h"
+#include "MENU.h"
+#include "FILE HANDLING.h"
+#include "PROGRESS.h"
 
-class Player
+int Player::random()
 {
-protected:
-    Map *m;
-    static int number;
-    static int x;
-    static int xI;
-    static int x1;
-    static int y;
-    static int yI;
-    static int y1;
-    static int score;
-    static int score1;
-    static bool flag;
-    static bool flag2;
-    static bool flag3;
-    static bool flag4;
-    static bool flag5;
-    static bool AI;
-    static bool bot;
-    int id = 0;
-    int id1 = 0;
-    static string name;
-    static bool Drawbullet;
-    static bool Drawbullet1;
-    static bool Drawbullet2;
-    static int level;
-    static int xS, yS;
-    static int xB, yB;
-
-    static bool mapy;
-    static bool mapy2;
-    static bool mapy1;
-    static bool car;
-
-public:
-    Player();
-    static int random();
-    void setName(string name);
-    int getID();
-    string Name();
-
-    void setScore(int score);
-    int getScore();
-
-    void draw1();
-    static void draw();
-    static void drawCar();
-
-    void moveCar();
-
-    static void AIshoot();
-    static void AImove();
-    static void shoot();
-    static void shoot1();
-    static void shoot2();
-    static void draw_bullet();
-    static void draw_bullet1();
-    static void draw_bullet2();
-
-    static int getRandomIntExcludingRange(int start, int end, int start_range, int end_range);
-    static void NonPrintableKeys(int key, int x, int y);
-    static int Timer(int m);
-    static int Timer1(int m);
-    static int Timer2(int m);
-    static void PrintableKeys(unsigned char key, int x, int y);
-    static void MouseClicked(int button, int state, int x, int y);
-};
-int Player::number = 0;
-int Player::x = 0;
-int Player::xS = 0;
-int Player::xB = 0;
-int Player::xI = 0;
-int Player::x1 = 0;
-int Player::y1 = 0;
-int Player::y = 0;
-int Player::yS = 0;
-int Player::yB = 0;
-int Player::yI = 0;
-int Player::score = 0;
-int Player::score1 = 0;
-int Player::level =random();
-bool Player::Drawbullet = false;
-bool Player::Drawbullet1 = false;
-bool Player::Drawbullet2 = false;
-bool Player::flag = true;
-bool Player::flag2 = true;
-bool Player::flag3 = true;
-bool Player::flag4 = true;
-bool Player::flag5 = true;
-bool Player::AI = true;
-bool Player::bot = false;
-bool Player::mapy = true;// random();
-bool Player::mapy1 = false;// random();
-bool Player::mapy2 = false;// random();
-bool Player::car = true;//random();
-string Player::name = "";
-
-
-class Progress: public Player
+    time_t t = time(0);
+    int result = t % 2;
+    cout << "Tinme: " << result << endl;
+    return result;
+}
+Map::Map()
 {
-private:
-    //string barstring;
-        
-public:
-    //Progress();
-    static void writeScore();
-    static void readScore();
-};
-class Map : public Progress
+    Mapid = 0;
+}
+
+Player::Player()
 {
-protected:
-    int Mapid;
+    // Map m;
+    name = "";
+    x = 100;
+    y = 400;
 
-public:
-    Map();
-    static void drawMap();
-    static void drawMap1();
-};
+    xI = 100;
+    yI = 400;
 
-class Menu : public Player
+    x1 = 900;
+    y1 = 400;
+
+    xS = xI;
+    xS = yI;
+    xB = x1;
+    yB = y1;
+    if (level == 0)
+    {
+        mapy = true;
+        car = true;
+    }
+    else
+    {
+        mapy = false;
+        car = false;
+    }
+}
+void Player::setName(string name)
 {
-protected:
-    int id;
+    this->name = name;
+}
+int Player::getID()
+{
+    return id;
+}
+string Player::Name()
+{
+    return name;
+}
+void Player::setScore(int score)
+{
+    score = score;
+}
+int Player::getScore()
+{
+    return score;
+}
 
-public:
-    Menu();
-    static void StartMenu();
-    static void botOrhuman();
-    static void GameOver();
-    static void ShowScores();
-    static void readFromFile();
-    static void writeIntoFile();
-};
+int Player::getRandomIntExcludingRange(int start, int end, int start_range, int end_range)
+{
+
+    // handle bad input gracefully
+    if (start > end)
+    {
+        std::swap(start, end);
+    }
+    if (start_range > end_range)
+    {
+        std::swap(start_range, end_range);
+    }
+    std::clamp(start_range, start, end);
+    std::clamp(end_range, start, end);
+
+    auto range_length = end_range - start_range;
+    auto adj_end = end - range_length;
+    auto shifted_end = adj_end - start;
+
+    int rand_int = (rand() % shifted_end) + start;
+
+    if (rand_int >= start_range)
+    {
+        rand_int = (rand_int - start_range) + end_range;
+    }
+    return rand_int;
+}
+
+void Player::draw()
+{
+    if (Player::level == 0)
+        Player::mapy = true;
+    else
+        Player::mapy = false;
+
+    glClearColor(0 /*Red Component*/, 0,                         // 148.0/255/*Green Component*/,
+                 0.0 /*Blue Component*/, 0 /*Alpha component*/); // Red==Green==Blue==1 --> White Colour
+    glClear(GL_COLOR_BUFFER_BIT);
+    drawCar();
+
+    if (mapy || bot || mapy1)
+    {
+        Map::drawMap();
+    }
+    else
+        Map::drawMap1();
+
+    if (Drawbullet)
+        draw_bullet();
+
+    if (Drawbullet1)
+    {
+        draw_bullet1();
+    }
+    if (Drawbullet2)
+    {
+        draw_bullet1();
+    }
+    glutSwapBuffers(); // do not modify this line..
+
+    glutPostRedisplay();
+}
+
+int Player::Timer1(int m)
+{
+    // condition
+    if (Drawbullet1)
+    {
+        shoot1();
+        glutTimerFunc(70, Player::Timer1, 0);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int Player::Timer2(int m)
+{
+    if (Drawbullet)
+    {
+        shoot();
+        glutTimerFunc(70, Player::Timer2, 0);
+    }
+    else
+    {
+        return 0;
+    }
+}
+int Player::Timer3(int m)
+{
+    if (Drawbullet2)
+    {
+        shoot2();
+        glutTimerFunc(70, Player::Timer3, 0);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int Player::Timer(int m)
+{
+
+    /* if (Drawbullet)
+        shoot(); */
+
+    /* if (Drawbullet1)
+    {
+        shoot1();
+    } */
+    /* if (Drawbullet2)
+    {
+        shoot2();
+        glutTimerFunc(100, Player::Timer, 0);
+
+    }
+    else
+    {
+      //  return 0;
+    } */
+    if (bot)
+    {
+        AImove();
+        glutTimerFunc(100, Player::Timer, 0);
+    }
+    else
+    {
+        return 0;
+    }
+
+    // once again we tell the library to call our Timer function after next 1000/FPS
+}
+
+void SetCanvasSize(int width, int height)
+{
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, width, 0, height, -1, 1); // set the screen size to given width and height.
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+int main(int argc, char *argv[])
+{
+    Menu menu;
+    Player p1, p2;
+    p1.setName("Ibraheem");
+    p2.setName("Abuzar");
+
+    int width = 1020, height = 840; // i have set my window size to be 800 x 600
+
+    InitRandomizer();                              // seed the random number generator...
+    glutInit(&argc, argv);                         // initialize the graphics library...
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);  // we will be using color display mode
+    glutInitWindowPosition(50, 50);                // set the initial position of our window
+    glutInitWindowSize(width, height);             // set the size of our window
+    glutCreateWindow("COMBAT by Ibraheem Rehman"); // set the title of our game window
+    SetCanvasSize(width, height);                  // set the number of pixels...
+
+    // Register your functions to the library,
+    // you are telling the library names of function to call for different tasks.
+    // glutDisplayFunc(display); // tell library which function to call for drawing Canvas.
+
+    glutSpecialFunc(Player::NonPrintableKeys); // tell library which function to call for non-printable ASCII characters
+    glutKeyboardFunc(Player::PrintableKeys);   // tell library which function to call for printable ASCII characters
+    glutDisplayFunc(menu.StartMenu);           // tell library which function to call for drawing Canvas.
+                                               // glutDisplayFunc(p2.drawCar); // tell library which function to call for drawing Canvas.
+
+    // glutTimerFunc(1000.0, Player::Timer, 0); //  This function tells the library to call our Timer function after 1000.0/FPS milliseconds...
+
+    glutMouseFunc(Player::MouseClicked);
+
+    // PlaySound(TEXT("mixkit-game-level-music-689.wav"), NULL, SND_ASYNC | SND_LOOP);
+
+    // now handle the control to library and it will call our registered functions when
+    // it deems necessary...
+    glutMainLoop();
+    return 1;
+}
